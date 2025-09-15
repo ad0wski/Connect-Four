2931366037e211ed22d54878e963ee082c9bee0a
Adam <ak.jurkowski@gmail.com>
2025-09-15 13:48:11 +0200

    Dodanie zmiany tury gracza i wyswietlanie planszy

--- Connect-Four-Game.py
+++ Connect-Four-Game.py
@@ -1,1 +1,29 @@
-"Code Here"
+plansza = [[" ", " ", " ", " ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " ", " ", " ", " "]]
+
+aktualnyGracz = "X"
+
+def zmianaGracza(aktualnyGracz):
+    if aktualnyGracz == "X":
+        aktualnyGracz = "0"
+    else:
+        aktualnyGracz = "X"
+    return aktualnyGracz
+
+def wyswietlaniePlanszy(plansza):
+        print("\n")
+        for i in range(6):
+            for j in range(7):
+                if j == 0:
+                     print(f"|{plansza[i][j]}", end=" |")
+                else:
+                     print(f"{plansza[i][j]}", end=" |")
+            print()
+        print("======================")
+        print(" 1  2  3  4  5  6  7")
+
+wyswietlaniePlanszy(plansza)
