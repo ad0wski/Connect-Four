8f5177ca40b168176c7cee738848c0a78edd1b5d
Adam <ak.jurkowski@gmail.com>
2025-09-16 14:19:40 +0200

    Dodanie mechaniki ruchu gracza

--- Connect-Four-Game.py
+++ Connect-Four-Game.py
@@ -27,3 +27,21 @@ def wyswietlaniePlanszy(plansza):
         print(" 1  2  3  4  5  6  7")
 
 wyswietlaniePlanszy(plansza)
+
+def ruchGracza(aktualnyGracz, plansza):
+    czyPoprawnieWstawionePole = False
+    while czyPoprawnieWstawionePole == False:
+        czyZajeteWszystkieMiejsca = True
+        wyborKolumny = int(input("Wybierz kolumnę: "))
+        kolumna = wyborKolumny - 1 #dajemy to jako index do planszy
+        if wyborKolumny > 0 and wyborKolumny < 8:
+            for i in range(5, -1, -1):
+                if plansza[i][kolumna] == " ":
+                    plansza[i][kolumna] = aktualnyGracz
+                    czyZajeteWszystkieMiejsca = False
+                    czyPoprawnieWstawionePole = True
+                    break
+            if czyZajeteWszystkieMiejsca == True:
+                print("Ta kolumna jest pełna! ")
+        else:
+            print("Wybierz kolumnę w zakresie 1 - 7")
